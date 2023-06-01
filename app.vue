d601087d88abf1baf2d67397be44d26f34517047
clspeter <mustpe@gmail.com>
2023-06-01 20:45:49 +0800

    add pages

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <Counter />
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="index">
+      <Counter />
+    </div>
+  </template>
+  
+  <style>
+  .index {
+    padding-top: 60px;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+  }
+  </style>
