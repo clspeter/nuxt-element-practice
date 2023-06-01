ff62dccb5ced650e81acf78f321c61283066f268
clspeter <mustpe@gmail.com>
2023-06-01 22:18:54 +0800

    add to google alert modal

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,16 @@
             </el-header>
             <el-main>
                 <Counter />
+                <br />
                 <el-switch v-model="colorMode" inline-prompt active-text="dark" inactive-text="light" size="large" />
+                <br />
+                <br />
+                <el-config-provider :locale="zhtw">
+                    <el-date-picker v-model="timeValue" type="date" placeholder="請選擇日期" />
+                </el-config-provider>
+                <br />
+                <br />
+                <el-button text @click="open">去Google.com</el-button>
             </el-main>
         </el-container>
     </div>
@@ -23,9 +32,43 @@
 }
 </style>
 <script setup lang="ts">
+import zhtw from "element-plus/es/locale/lang/zh-tw";
+import { ElMessage, ElMessageBox } from 'element-plus'
+
+const open = () => {
+    ElMessageBox.confirm(
+        '這會將您重新導向至Google',
+        '確定要前往Google嗎?',
+        {
+            confirmButtonText: '是',
+            cancelButtonText: '否',
+            type: 'warning',
+        }
+    )
+        .then(() => {
+            ElMessage({
+                type: 'success',
+                message: '前往Google',
+            })
+            setTimeout(() => {
+                window.location.href = "https://www.google.com";
+            }, 1000);
+        })
+        .catch(() => {
+            ElMessage({
+                type: 'info',
+                message: '取消',
+            })
+        })
+}
+
 const color = useColorMode();
 const colorMode = computed({
     get: () => color.value === 'dark',
     set: () => (color.preference = color.value === 'dark' ? 'light' : 'dark'),
 });
+
+
+
+const timeValue = ref("");
 </script>
