8ff225de74b8639bc7a786ecfe47361ce94bf0a2
clspeter <mustpe@gmail.com>
2023-06-02 21:23:30 +0800

    add layouts for header

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+import { NuxtLink } from '../.nuxt/components';
+<template>
+    <div class="bg-gray-600">
+        <header>
+            <nav class="flex py-6 px-4 space-x-10">
+                <NuxtLink class="text-green-500 text-underline" to="/">Nuxt Home</NuxtLink>
+                <NuxtLink class="text-green-500 text-underline" to="/pomodoro">Pomodoro</NuxtLink>
+                <NuxtLink class="text-green-500 " to="/about">About</NuxtLink>
+            </nav>
+        </header>
+    </div>
+    <div>
+        <slot />
+    </div>
+</template>
+
---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="py-24">
+        <div class="flex flex-col items-center">
+            <h1 class="text-3xl text-green-600 ">大家好，我是Peter</h1>
+        </div>
+    </div>
+</template>
