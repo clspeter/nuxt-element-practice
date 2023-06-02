40bdca01d53cd1e7d975e379a4b6f36f0a4390ec
clspeter <mustpe@gmail.com>
2023-06-02 20:42:33 +0800

    add tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@element-plus/nuxt', '@nuxtjs/color-mode'],
+    modules: ['@element-plus/nuxt', '@nuxtjs/color-mode', '@nuxtjs/tailwindcss'],
     css: ["@/assets/scss/index.scss"],
     colorMode: {
         preference: 'light',
