d5df9a44f5de3a48648088b18348d885fb6213dc
clspeter <mustpe@gmail.com>
2023-06-02 22:41:50 +0800

    pomodoro style

--- pages/pomodoro.vue
+++ pages/pomodoro.vue
@@ -1,22 +1,74 @@
 <template>
-    <title>Pomodoro Technique</title>
-    <meta name="description" content="Pomodoro Technique" />
-    <link rel="icon" href="/favicon.ico" />
-    <div>
-        Pomodoro Technique
+    <div class="grow bg-gray-800 text-white">
+        <div class="mx-auto mt-10 flex w-screen flex-col gap-2 rounded bg-emerald-700 md:max-w-xl p-4">
+            <div class="text-center text-3xl">Pomodoro Technique</div>
+            <div class="flex flex-row justify-center text-2xl">
+                <div class="basis-32">
+                    <div class="text-center" id="break-label">Break</div>
+                    <div class="flex flex-row items-center justify-center">
+                        <button id="break-decrement" class="h-[24px] rounded bg-emerald-800 active:bg-emerald-500">
+                            -
+                        </button>
+                        <div id="break-length" class="mx-2">{{ breakLength }}</div>
+                        <button id="break-increment" class="h-[24px] rounded bg-emerald-800 active:bg-emerald-500">
+                            +
+                        </button>
+                    </div>
+                </div>
+                <div class="basis-32">
+                    <div class="text-center" id="session-label">Session</div>
+                    <div class="flex flex-row items-center justify-center">
+                        <button id="session-decrement" class="h-[24px] rounded bg-emerald-800 active:bg-emerald-500">
+                            -
+                        </button>
+                        <div id="session-length" class="mx-2">{{ sessionLength }}</div>
+                        <button id="session-increment" class="h-[24px] rounded bg-emerald-800 active:bg-emerald-500">
+                            +
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="mx-auto flex w-36 flex-col rounded border-2 border-emerald-800 py-2">
+                <div class="text-center" id="timer-label">{{ isBreak ? 'Break' : 'Session' }}</div>
+                <div class="text-center text-5xl" id="time-left">{{ timerString }}</div>
+                <audio src="/time-up-ringtone.mp3" id="beep" ref="audioRef"></audio>
+            </div>
+            <div class="mb-2 flex flex-row justify-center gap-4">
+                <button class="rounded bg-emerald-800 active:bg-emerald-500" id="start_stop">
+                    <div>Start</div>
+                </button>
+                <button class="rounded bg-emerald-800 active:bg-emerald-500" id="reset">
+                    Reset
+                </button>
+            </div>
+        </div>
     </div>
 </template>
+  
+<script>
+import { ref } from 'vue';
+import { CoPlus, CoMinus } from "oh-vue-icons/icons";
 
-<script lang="ts">
-import { defineComponent } from 'vue'
 
-export default defineComponent({
+export default {
     setup() {
+        const breakLength = ref(5);
+        const sessionLength = ref(25);
+        const timerString = ref('25:00');
+        const isBreak = ref(false);
+        const isRunning = ref(false);
+        const audioRef = ref(null);
+        let timer;
 
 
-        return {}
-    }
-})
+        return {
+            breakLength,
+            sessionLength,
+            timerString,
+            isBreak,
+            isRunning,
+            audioRef,
+        };
+    },
+};
 </script>
-
-<style scoped></style>
