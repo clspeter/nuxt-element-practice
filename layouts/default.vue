b405ace79c49584938fb8d66cdaedd7116056db4
clspeter <mustpe@gmail.com>
2023-06-02 21:46:28 +0800

    update about

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,17 @@
 import { NuxtLink } from '../.nuxt/components';
 <template>
-    <div class="bg-gray-600">
-        <header>
-            <nav class="flex py-6 px-4 space-x-10">
-                <NuxtLink class="text-green-500 text-underline" to="/">Nuxt Home</NuxtLink>
-                <NuxtLink class="text-green-500 text-underline" to="/pomodoro">Pomodoro</NuxtLink>
-                <NuxtLink class="text-green-500 " to="/about">About</NuxtLink>
-            </nav>
-        </header>
-    </div>
-    <div>
+    <div class="min-h-screen flex flex-col">
+        <div class="bg-gray-600">
+            <header>
+                <nav class="flex py-6 px-4 space-x-10 justify-between">
+                    <NuxtLink class="text-green-500 text-underline" to="/">Hero Home</NuxtLink>
+                    <div class="space-x-10">
+                        <NuxtLink class="text-green-500 text-underline" to="/pomodoro">Pomodoro</NuxtLink>
+                        <NuxtLink class="text-green-500 " to="/about">About</NuxtLink>
+                    </div>
+                </nav>
+            </header>
+        </div>
         <slot />
     </div>
 </template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="py-24">
-        <div class="flex flex-col items-center">
-            <h1 class="text-3xl text-green-600 ">大家好，我是Peter</h1>
+    <div class="py-24 bg-gray-800 grow">
+        <div class="flex">
+            <div class="items-center rounded bg-gray-600 mx-auto py-6 px-4">
+                <div class="text-green-400">
+                    這是一個練習使用Vue3的專案
+                    <p>使用技術: Nuxt3 + Tailwind CSS</p>
+                    <p>使用了nuxt 的 layout來做出共用的header</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
