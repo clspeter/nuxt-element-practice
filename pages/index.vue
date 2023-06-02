5bbb5868525d6cb08894bc003bd53b7dfa985e63
clspeter <mustpe@gmail.com>
2023-06-02 21:35:31 +0800

    add typewriter for index

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-    <div>this is index</div>
+    <div class="grow w-full bg-gray-800 flex">
+        <div class="m-auto">
+            <VueWriter class=" text-3xl text-green-600 font-semibold"
+                :array="['<This_is_Peter/>', 'WhoSartedToLearnVue.js']" />
+        </div>
+    </div>
 </template>
+
+<script>
+import VueWriter from 'vue-writer'
+export default {
+    components: { VueWriter }
+}
+</script>
