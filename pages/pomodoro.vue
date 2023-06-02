39bca15613eaf1df0cbb07face4b59110df90425
clspeter <mustpe@gmail.com>
2023-06-02 21:55:36 +0800

    pomodoro init

---
+++ pages/pomodoro.vue
@@ -0,0 +1,22 @@
+<template>
+    <title>Pomodoro Technique</title>
+    <meta name="description" content="Pomodoro Technique" />
+    <link rel="icon" href="/favicon.ico" />
+    <div>
+        Pomodoro Technique
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+
+
+        return {}
+    }
+})
+</script>
+
+<style scoped></style>
