0503d117ae1c03acb85fdca81bddf696b76f24c7
clspeter <mustpe@gmail.com>
2023-06-02 23:56:48 +0800

    timer logic

--- pages/pomodoro.vue
+++ pages/pomodoro.vue
@@ -6,15 +6,15 @@
                 <div class="basis-32">
                     <div class="text-center" id="break-label">Break</div>
                     <div class="flex flex-row items-center justify-center">
-                        <button @click="decrementBreakLength" id="break-decrement"
-                            class="h-[24px] rounded bg-emerald-800 active:bg-emerald-500">
+                        <button :disabled="isStarted" @click="decrementBreakLength" id="break-decrement"
+                            class="h-[24px] rounded bg-emerald-800 active:bg-emerald-500 disabled:pointer-events-none disabled:opacity-50">
                             <el-icon>
                                 <Minus />
                             </el-icon>
                         </button>
                         <div id="break-length" class="mx-2">{{ breakLength }}</div>
-                        <button @click="incrementBreakLength" id="break-increment"
-                            class="h-[24px] rounded bg-emerald-800 active:bg-emerald-500">
+                        <button :disabled="isStarted" @click="incrementBreakLength" id="break-increment"
+                            class="h-[24px] rounded bg-emerald-800 active:bg-emerald-500 disabled:pointer-events-none disabled:opacity-50">
                             <el-icon>
                                 <Plus />
                             </el-icon>
@@ -24,15 +24,15 @@
                 <div class="basis-32">
                     <div class="text-center" id="session-label">Session</div>
                     <div class="flex flex-row items-center justify-center">
-                        <button @click="decrementSessionLength" id="session-decrement"
-                            class="h-[24px] rounded bg-emerald-800 active:bg-emerald-500">
+                        <button :disabled="isStarted" @click="decrementSessionLength" id="session-decrement"
+                            class="h-[24px] rounded bg-emerald-800 active:bg-emerald-500 disabled:pointer-events-none disabled:opacity-50">
                             <el-icon>
                                 <Minus />
                             </el-icon>
                         </button>
                         <div id="session-length" class="mx-2">{{ sessionLength }}</div>
-                        <button @click="incrementSessionLength" id="session-increment"
-                            class="h-[24px] rounded bg-emerald-800 active:bg-emerald-500">
+                        <button :disabled="isStarted" @click="incrementSessionLength" id="session-increment"
+                            class="h-[24px] rounded bg-emerald-800 active:bg-emerald-500 disabled:pointer-events-none disabled:opacity-50">
                             <el-icon>
                                 <Plus />
                             </el-icon>
@@ -46,16 +46,16 @@
                 <audio src="/time-up-ringtone.mp3" id="beep" ref="audioRef"></audio>
             </div>
             <div class="mb-2 flex flex-row justify-center gap-4">
-                <button class="rounded bg-emerald-800 active:bg-emerald-500 h-[24px]" id="start_stop">
-                    <el-icon size="24">
+                <button @click="toggleTimer" class="rounded bg-emerald-800 active:bg-emerald-500 h-[40px]" id="start_stop">
+                    <el-icon v-show="!isRunning" size="40">
                         <VideoPlay />
                     </el-icon>
-                    <el-icon size="24">
+                    <el-icon v-show="isRunning" size="40">
                         <VideoPause />
                     </el-icon>
                 </button>
-                <button class="rounded bg-emerald-800 active:bg-emerald-500 h-[24px]" id="reset">
-                    <el-icon size="24">
+                <button @click="resetTimer" class="rounded bg-emerald-800 active:bg-emerald-500 h-[40px]" id="reset">
+                    <el-icon size="40">
                         <Refresh />
                     </el-icon>
                 </button>
@@ -76,6 +76,7 @@ export default {
         const timerString = ref('25:00');
         const isBreak = ref(false);
         const isRunning = ref(false);
+        const isStarted = ref(false);
         const audioRef = ref(null);
         let timer;
 
@@ -104,18 +105,79 @@ export default {
             }
         };
 
+        const toggleTimer = () => {
+            isStarted.value = true;
+            isRunning.value = !isRunning.value;
+            if (isRunning.value) {
+                startTimer();
+            } else {
+                stopTimer();
+            }
+        };
+
+        const startTimer = () => {
+            const totalSeconds = isBreak.value ? breakLength.value * 60 : sessionLength.value * 60;
+            let secondsLeft = totalSeconds;
+
+            timer = setInterval(() => {
+                secondsLeft--;
+
+                const minutes = Math.floor(secondsLeft / 60);
+                const seconds = secondsLeft % 60;
+                timerString.value = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+
+                if (secondsLeft === 0) {
+                    clearInterval(timer);
+                    isBreak.value = !isBreak.value;
+                    audioRef.value.play();
+                    if (isBreak.value) {
+                        timerString.value = `${breakLength.value}:00`;
+                    } else {
+                        timerString.value = `${sessionLength.value}:00`;
+                    }
+                    startTimer();
+                }
+            }, 1000);
+        };
+
+        const stopTimer = () => {
+            clearInterval(timer);
+        };
+
+        const resetTimer = () => {
+            clearInterval(timer);
+            isRunning.value = false;
+            isBreak.value = false;
+            isStarted.value = false;
+            breakLength.value = 5;
+            sessionLength.value = 25;
+            timerString.value = '25:00';
+            audioRef.value.pause();
+            audioRef.value.currentTime = 0;
+        };
+
+        watch([sessionLength], () => {
+            if (!isBreak.value) {
+                timerString.value = `${sessionLength.value}:00`;
+            } else {
+                timerString.value = `${breakLength.value}:00`;
+            }
+        });
 
         return {
             breakLength,
             sessionLength,
             timerString,
             isBreak,
+            isStarted,
             isRunning,
             audioRef,
             decrementBreakLength,
             incrementBreakLength,
             decrementSessionLength,
             incrementSessionLength,
+            toggleTimer,
+            resetTimer,
         };
     },
 };
