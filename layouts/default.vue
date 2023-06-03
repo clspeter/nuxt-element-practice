5c5debb94d74f7bf46e1573ed7873978ef78e1e7
clspeter <mustpe@gmail.com>
2023-06-03 12:05:41 +0800

    style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 import { NuxtLink } from '../.nuxt/components';
 <template>
     <div class="min-h-screen flex flex-col">
-        <div class="bg-primary">
+        <div class="bg-rose-200 shadow-md">
             <header>
-                <nav class="flex py-6 px-4 space-x-10 justify-between text-neutral">
-                    <NuxtLink class=" text-underline" to="/">Hero Home</NuxtLink>
-                    <div class="space-x-10">
+                <nav class="flex py-4 px-6 space-x-10 justify-between text-accent">
+                    <NuxtLink class="text-underline font-bold" to="/">Hero Home</NuxtLink>
+                    <div class="space-x-6 font-bold">
                         <NuxtLink class="text-underline" to="/pomodoro">Pomodoro</NuxtLink>
                         <NuxtLink class="" to="/about">About</NuxtLink>
                     </div>
@@ -13,8 +13,8 @@ import { NuxtLink } from '../.nuxt/components';
             </header>
         </div>
         <slot />
-        <div class="bg-primary">
-            <footer class="flex py-6 px-4 space-x-10 justify-end text-neutral">
+        <div class="bg-rose-200 shadow-">
+            <footer class="flex py-2 px-4 justify-end text-accent">
                 <NuxtLink class="text-underline" to="https://github.com/clspeter/nuxt-element-practice">
                     Made by clspeter
                 </NuxtLink>
